c85e923d795d4844b930ac67119c1972585dc5b1
RorySchof <roryschofield@gmail.com>
2023-03-15 16:39:46 -0400

    added wasd key functionality

--- input.js
+++ input.js
@@ -1,12 +1,31 @@
-const handleUserInput = function (key) {
+let connection;
 
-  if (key === '\u0003') {
+
+const handleUserInput = function (key) {
+console.log("key", key)
+ /* if (key == '\u0003') {
     console.log("handle user input")
+    process.exit()
     }
+    */
+
+    if (key === 'w') {
+      console.log("Move: up")}
+    
+      if (key === 'a') {
+        console.log("Move: left")}
+    
+        if (key === 's') {
+          console.log("Move: right")}
+    
+           if (key === 'w') {
+            console.log("Move: down")}
+    
 };
 
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,4 +35,4 @@ const setupInput = function () {
   return stdin;
 };
 
-module.exports={setupInput}
+module.exports = setupInput
